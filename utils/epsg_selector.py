a65f9e61fc49939df0c2cb9fee9e1b4ca4d0cf0b
The Fox coder <58402974+jclpaiva@users.noreply.github.com>
2024-12-29 14:32:57 +0000

    Add files via upload

---
+++ utils/epsg_selector.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+def get_epsg_options():
+    """Read the EPSG codes from the CSV file and return a dictionary of options."""
+    # Path to the CSV file
+    csv_path = os.path.join(os.path.dirname(__file__), "data", "codes.csv")
+    
+    # Read the CSV file with the correct encoding (ISO-8859-1)
+    df = pd.read_csv(csv_path, sep=";", encoding="ISO-8859-1")
+    
+    # Create a dictionary of EPSG options
+    #epsg_options = {}
+    #for _, row in df.iterrows():
+    #    region = row["Region"]
+    #    epsg_code = row["EPSG"]
+    #    epsg_options[f"{region}: {epsg_code}"] = epsg_code
+
+    epsg_options = {}
+    for _, row in df.iterrows():
+        epsg_code = row["EPSG"]
+        system = row["System"]
+        region = row["Region"]
+        epsg_options[f"{epsg_code} - {system} - {region} "] = epsg_code
+    return epsg_options
